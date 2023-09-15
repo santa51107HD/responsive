944ff1338a408fa6c5ac927c69ed1dae59e52e50
Juan Camilo Santa Gomez <santa51107@gmail.com>
2023-09-15 16:47:16 -0500

    Imagenes expandidas

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,23 @@ import './App.css';
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
   const menuRef = useRef(null);
+
+  /*Esta parte es para el despliegue de las imagenes*/
+
+  const toggleLightbox = (image) => {
+    setSelectedImage(image);
+    setIsLightboxOpen(!isLightboxOpen);
+    console.log("imagen")
+  };
+
+  const closeLightbox = () => {
+    setIsLightboxOpen(false);
+  };
+
+  /* Aqui acaba */
 
   const toggleMenu = () => {
     // Detener la propagación del evento para que no llegue al documento y que no se cierre el menu debido al closeMenu
@@ -52,18 +68,48 @@ function App() {
         </div>
       </header>
       <main>
-        <section className='contenedor' id='servicio'></section>
+        <section className='contenedor' id='servicio'>
           <h2 className='subtitulo'>Nuestro Servicio</h2>
           <div className='contenedor-servicio'>
             <img src='../iconos/Checklist_Isometric.svg' alt='Servicio'></img>
             <div className='checklist-servicio'>
               <div className='service'>
-                <h3 class="n-service"><span className='number'>1</span>Diseño Web</h3>
+                <h3 className="n-service"><span className='number'>1</span> Diseño Web</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo molestias iure minus quo quaerat cum maiores, voluptatum error dolor impedit.</p>
+              </div>
+              <div className='service'>
+                <h3 className="n-service"><span className='number'>2</span> Diseño Web</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo molestias iure minus quo quaerat cum maiores, voluptatum error dolor impedit.</p>
+              </div>
+              <div className='service'>
+                <h3 className="n-service"><span className='number'>3</span> Diseño Web</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo molestias iure minus quo quaerat cum maiores, voluptatum error dolor impedit.</p>
               </div>
             </div>
           </div>
-        <section></section>
-        <section></section>
+        </section>
+        <section className='gallery' id='portafolio'>
+            <div className="contenedor">
+              <h2 className='subtitulo'>Galeria</h2>
+              <div className="contenedor-galeria">
+                <img src='../img/1.jpg' className='img-galeria' onClick={() => toggleLightbox('../img/1.jpg')}></img>
+                <img src='../img/2.jpg' className='img-galeria' onClick={() => toggleLightbox('../img/2.jpg')}></img>
+                <img src='../img/3.jpg' className='img-galeria' onClick={() => toggleLightbox('../img/3.jpg')}></img>
+                <img src='../img/4.jpg' className='img-galeria' onClick={() => toggleLightbox('../img/4.jpg')}></img>
+                <img src='../img/5.jpg' className='img-galeria' onClick={() => toggleLightbox('../img/5.jpg')}></img>
+                <img src='../img/6.jpg' className='img-galeria' onClick={() => toggleLightbox('../img/6.jpg')}></img>
+              </div>
+            </div>
+        </section>
+        <section className={`image-light ${isLightboxOpen ? 'show' : ''}`}>
+        <img
+          src='../iconos/bx-x.svg'
+          className='close'
+          onClick={closeLightbox}
+          alt='Cerrar'
+        />
+        <img src={selectedImage} className='agregar-imagen' alt='Imagen en lightbox' />
+      </section>
       </main>
     </>
   );
